79ca64634b50b209ea29dd7f4ee12403aadb8353
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-10-18 21:28:45 +0200

    404

--- src/router/index.js
+++ src/router/index.js
@@ -18,14 +18,15 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
+    
 
 })
 
-router.beforeEach((to, from) => {
-    console.log(to, from)
-    nextTick()
-    return true
-})
+// router.beforeEach((to, from) => {
+//     console.log(to, from)
+//     nextTick()
+//     return true
+// })
 
 
 console.log('router: ', import.meta.env.BASE_URL)
